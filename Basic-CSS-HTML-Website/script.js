67d36cc8c58832b5f9e5122723f53ca4d1fe4998
Rajasi <mihikarm@gmail.com>
2026-09-02 19:17:26 -0400

    Add files via upload

---
+++ Basic-CSS-HTML-Website/script.js
@@ -0,0 +1,11 @@
+function ClickHello() {
+    window.location.href = "https://forms.hackclub.com/sunbeam-signup?referral_code=229";
+}
+
+function nextPage() {
+    window.location.href = "second.html";
+}
+
+function previousPage() {
+    window.location.href = "index.html";
+}
